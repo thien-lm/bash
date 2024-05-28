a7da9e26eff233faf74ade3535e3b2217b0d5c25
thien-lm <thien.lm204608@sis.hust.edu.vn>
2024-05-28 10:26:47 +0700

    fix deployment and bash

--- reboot_to_specific_kernel.sh
+++ reboot_to_specific_kernel.sh
@@ -1,20 +1,71 @@
-# Thanh Tran Viet
-#dpkg -l | grep linux-image
-apt-mark hold linux-image-5.15.0-76-generic
-apt-mark hold linux-image-5.15.0-107-generic
+#!/bin/bash
+# Copyright 2023 FPT Cloud - PaaS
 
-KERNELVER=5.15.0-76-generic
-MID=$(awk '/Advanced options for Ubuntu/{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2)
-KID=$(awk "/with Linux $KERNELVER/"'{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2 | head -n1)
+set -o errexit
+set -o pipefail
+set -u
 
-# echo "GRUB_DEFAULT='${MID}>${KID}'" >> /etc/default/grub
-# or prefer put it in a seprate file
-cat > /etc/default/grub.d/95-savedef.cfg <<EOF
-GRUB_DEFAULT=saved
-GRUB_SAVEDEFAULT=true
-EOF
-firstoutput=$(grub-editenv /boot/grub/grubenv set saved_entry="${MID}>${KID}")
-secondoutput=$(update-grub)
-echo $firstoutput
-echo $secondoutput
+set -x
+
+INFRA_PLATFORM="${INFRA_PLATFORM:-VMW}" #VMW/OSP
+# KEEP_KERNEL="${KEEP_KERNEL:-5.15.0-76}"
+BLOCK_KERNEL="${BLOCK_KERNEL:-5.15.0-107}"
+#GRUB_FILE_CONFIG="${GRUB_FILE_CONFIG:-/boot/grub/grub.cfg}"
+
+set +x
+
+# check_cache() {
+#   echo "Checking cached version"
+#   if [[ ! -f "${CACHE_FILE}" ]]; then
+#     echo "Cache file ${CACHE_FILE} not found."
+#     return 1
+#   fi
+# }
+
+init_kernel_version() {
+  echo "Checking Infra Platform"
+  if [[ "${INFRA_PLATFORM}" == "VMW" ]]; then
+    echo "Infra Platform using VMware"
+    KEEP_KERNEL="5.15.0-76"
+  else
+    KEEP_KERNEL="5.15.0-88"
+  fi
+}
+
+prevent_upgrade_kernel() {
+  echo "Prevent upgrade OS kernel"
+  apt-mark hold linux-image-${KEEP_KERNEL}-generic
+  apt-mark hold linux-image-${BLOCK_KERNEL}-generic
+}
+
+update_grub_config() {
+  MID=$(awk '/Advanced options for Ubuntu/{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2)
+  KID=$(awk "/with Linux $KEEP_KERNEL/"'{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2 | head -n1)
+
+  cat > /etc/default/grub.d/95-savedef.cfg < EOF
+  GRUB_DEFAULT=saved
+  GRUB_SAVEDEFAULT=true
+  EOF
+  grub-editenv /boot/grub/grubenv set saved_entry="${MID}>${KID}"
+  update-grub
+}
+
+check_kernel_version() {
+  dpkg -l | grep linux-image
+  apt-get update && apt-get install -y linux-headers-${KERNEL_VERSION}
+  echo "Downloading kernel sources... DONE."
+}
+
+main() {
+  first_output=$(init_kernel_version)
+  second_output=$(prevent_upgrade_kernel)
+  third_output=$(update_grub_config)
+  fourth_output=$(check_kernel_version)
+  echo $first_output
+  echo $second_output
+  echo $third_output
+  echo $fourth_output
+}
+
+main "$@"
 sleep infinity
