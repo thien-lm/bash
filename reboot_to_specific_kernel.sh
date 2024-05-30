462876b881c4d278cfa917638c84f701712ef858
thien-lm <thien.lm204608@sis.hust.edu.vn>
2024-05-30 08:48:02 +0700

    update script and daemonset

--- reboot_to_specific_kernel.sh
+++ reboot_to_specific_kernel.sh
@@ -7,28 +7,45 @@ set -u
 
 set -x
 
+PREVENT_UPGRADE_DIR="${PREVENT_UPGRADE_DIR:-/var/cache}"
+CACHE_FILE="${PREVENT_UPGRADE_DIR}/.cache"
 INFRA_PLATFORM="${INFRA_PLATFORM:-VMW}" #VMW/OSP
-# KEEP_KERNEL="${KEEP_KERNEL:-5.15.0-76}"
 BLOCK_KERNEL="${BLOCK_KERNEL:-5.15.0-107}"
+REVERT_OPERATION="${REVERT_OPERATION:-true}"
+# KEEP_KERNEL="${KEEP_KERNEL:-5.15.0-76}"
 #GRUB_FILE_CONFIG="${GRUB_FILE_CONFIG:-/boot/grub/grub.cfg}"
-
 set +x
 
-# check_cache() {
-#   echo "Checking cached version"
-#   if [[ ! -f "${CACHE_FILE}" ]]; then
-#     echo "Cache file ${CACHE_FILE} not found."
-#     return 1
-#   fi
-# }
+check_cached_version() {
+  echo "Checking cached version"
+  if [[ ! -f "${CACHE_FILE}" ]]; then
+    echo "Cache file ${CACHE_FILE} not found."
+    return 1
+  fi
+  . "${CACHE_FILE}"
+  if [[ "${BLOCK_KERNEL}" == "${CACHE_BLOCK_KERNEL_VERSION}" ]]; then
+    echo "Found existing kernel reverter for latest kernel version ${BLOCK_KERNEL}."
+    return 0
+  fi
+  echo "Cache file ${CACHE_FILE} found but existing kernel versions didn't match."
+  return 1
+}
+
+update_cached_version() {
+  cat >"${CACHE_FILE}"<<__EOF__
+  CACHE_BLOCK_KERNEL_VERSION=${BLOCK_KERNEL}
+__EOF__
+  echo "Updated cached version as:"
+  cat "${CACHE_FILE}"
+}
 
 init_kernel_version() {
   echo "Checking Infra Platform"
   if [[ "${INFRA_PLATFORM}" == "VMW" ]]; then
     echo "Infra Platform using VMware"
-    KEEP_KERNEL="5.15.0-76"
-  else
     KEEP_KERNEL="5.15.0-88"
+  else
+    KEEP_KERNEL="5.15.0-76"
   fi
 }
 
@@ -42,27 +59,48 @@ update_grub_config() {
   MID=$(awk '/Advanced options for Ubuntu/{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2)
   KID=$(awk "/with Linux $KEEP_KERNEL/"'{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2 | head -n1)
 
-  cat > /etc/default/grub.d/95-savedef.cfg << EOF
+  cat > /etc/default/grub.d/95-savedef.cfg <<__EOF__
   GRUB_DEFAULT=saved
   GRUB_SAVEDEFAULT=true
-  EOF
+__EOF__
   grub-editenv /boot/grub/grubenv set saved_entry="${MID}>${KID}"
   update-grub
 }
 
 check_kernel_version() {
   dpkg -l | grep linux-image
-  apt-get update && apt-get install -y linux-headers-${KERNEL_VERSION}
-  echo "Downloading kernel sources... DONE."
+  uname -r
+  uname -a 
+  echo "Kernel version is: linux-image-$KERNEL_VERSION"
 }
 
-main() {
-  init_kernel_version
-  prevent_upgrade_kernel
-  output=$(update_grub_config)
-  output2=$(check_kernel_version)
-  echo $output
-  echo $output2
+revert_script_action() {
+  echo "reverting action performed by this script..."
+  echo "unholding kernel version.."
+  apt-mark unhold linux-image-"${KEEP_KERNEL}"-generic
+  apt-mark unhold linux-image-"${BLOCK_KERNEL}"-generic
+  echo "removing cache file..."
+  rm "${CACHE_FILE}"
+  echo "revert boot entry..."
+  rm /etc/default/grub.d/95-savedef.cfg
+  grub-editenv /boot/grub/grubenv unset saved_entry
+  update-grub
+  echo "reverted action performed by this script"
+}
+
+main() { 
+  if [[ "${REVERT_OPERATION}" == "true" ]]; then
+    init_kernel_version
+    revert_script_action
+  elif check_cached_version; then
+    check_kernel_version
+  else
+    init_kernel_version
+    prevent_upgrade_kernel
+    update_grub_config
+    update_cached_version
+    check_kernel_version
+  fi
 }
 
 main "$@"
